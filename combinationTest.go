a7e14f1d3a1ce862febc33511ba720a06408d8ac
ghbarton <george@barton.me>
2024-05-29 21:55:13 +0100

    feat: create initial tests

--- combinationTest.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "testing"
-
-func TestCombinations(t *testing.T) {
-	combinations := []string{"x", "y"}
-	expected := "xy, yx"
-
-}
---
+++ combination_test.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type ExampleTestSuite struct {
+	suite.Suite
+}
+
+func TestExampleTestSuite(t *testing.T) {
+	suite.Run(t, &ExampleTestSuite{})
+}
+
+func TestCombinations(t *testing.T) {
+	asserts := assert.New(t)
+	testCases := []struct {
+		testName  string
+		input     []string
+		index     int
+		expOutput []string
+		err       error
+	}{
+		{
+			testName: "Ensure func removes middle element",
+			input: []string{
+				"zero",
+				"one",
+				"two",
+			},
+			index: 1,
+			expOutput: []string{
+				"zero",
+				"two",
+			},
+			err: nil,
+		},
+		{
+			testName: "Ensure func removes last element",
+			input: []string{
+				"zero",
+				"one",
+				"two",
+			},
+			index: 2,
+			expOutput: []string{
+				"zero",
+				"one",
+			},
+			err: nil,
+		},
+		{
+			testName: "Ensure func removes first element",
+			input: []string{
+				"zero",
+				"one",
+				"two",
+			},
+			index: 0,
+			expOutput: []string{
+				"one",
+				"two",
+			},
+			err: nil,
+		},
+		{
+			testName:  "Ensure empty array returns error",
+			input:     []string{},
+			index:     0,
+			expOutput: []string(nil),
+			err:       errors.New("Cannot split an empty array"),
+		},
+		{
+			testName:  "Ensure an array of a single item returns an error",
+			input:     []string{
+				"Zero"
+			},
+			index:     0,
+			expOutput: []string(nil),
+			err:       errors.New("Cannot split an empty array"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			audit, err := RemoveElement(tc.input, tc.index)
+			asserts.Equal(tc.expOutput, audit)
+			asserts.Equal(tc.err, err)
+		})
+	}
+}
